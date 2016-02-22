ba60df96a0e93fdbe47d67b74001d835474d865e
Kup9_NpS <Kirich_s4@mail.ru>
2016-02-23 00:07:56 +0500

    added images and bootstrap

--- posts/views.py
+++ posts/views.py
@@ -1,15 +1,26 @@
-from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.contrib import messages
+from django.shortcuts import render, get_object_or_404, redirect
+from django.http import HttpResponse, HttpResponseRedirect
 from .models import Post
+from .forms import PostForm
 
 
 # Create your views here.
 def post_create(request):
-    return HttpResponse("<h1>Create</h1>")
+    form = PostForm(request.POST or None, request.FILES or None)
+    if form.is_valid():
+        instance = form.save(commit=False)
+        instance.save()
+        messages.success(request, "Sucessfully Created")
+        return HttpResponseRedirect(instance.get_absolute_url())
+    context = {
+        'form': form,
+    }
+    return render(request, "post_form.html", context)
 
 
 def post_detail(request,id):
-    # instance = Post.objects.get(id=3)
     instance = get_object_or_404(Post, id=id)
     context ={
         "instance": instance,
@@ -19,27 +30,45 @@ def post_detail(request,id):
 
 
 def post_list(request):
-    queryset = Post.objects.all()
+    queryset_list = Post.objects.all()
+    paginator = Paginator(queryset_list, 5) # Show 25 contacts per page
+    page = request.GET.get('page')
+    try:
+        queryset = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        queryset = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        queryset = paginator.page(paginator.num_pages)
     context ={
         "object_list": queryset,
         "title": "List User"
     }
-    # if request.user.is_authenticated():
-    #     context ={
-    #     "title": "List User"
-    # }
-    # else:
-    #     context ={
-    #     "title": "List anon"
-    # }
 
-    return render(request, "index.html", context)
-    # return HttpResponse("<h1>List</h1>")
+    return render(request, "post_list.html", context)
 
 
-def post_update(request):
-    return HttpResponse("<h1>Update</h1>")
 
 
-def post_delete(request):
-    return HttpResponse("<h1>Delete</h1>")
+
+def post_update(request, id=None):
+    instance = get_object_or_404(Post, id=id)
+    form = PostForm(request.POST or None,request.FILES or None, instance=instance)
+    if form.is_valid():
+        instance = form.save(commit=False)
+        instance.save()
+        messages.success(request, "Sucessfully Saved")
+        return HttpResponseRedirect(instance.get_absolute_url())
+    context ={
+        "instance": instance,
+        "form":form
+    }
+    return render(request, "post_form.html", context)
+
+
+def post_delete(request, id=None):
+    instance = get_object_or_404(Post, id=id)
+    instance.delete()
+    messages.success(request, "Sucessfully deleted")
+    return redirect("posts:list")
