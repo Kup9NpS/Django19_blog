04b3e779be458137fb18690f68ea07f600dac55d
Kup9_NpS <Kirich_s4@mail.ru>
2016-02-22 12:24:48 +0500

    initial

---
+++ posts/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class Kup9BlogConfig(AppConfig):
+    name = 'posts'
---
+++ posts/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import url
+from .views import post_list, post_create, \
+    post_detail, post_update, post_delete
+
+
+urlpatterns = [
+    url(r'^$', post_list),
+    url(r'^create/$',post_create),
+    url(r'^(?P<id>\d+)$',post_detail),
+    url(r'^update/$', post_update),
+    url(r'^delete/$', post_delete),
+]
